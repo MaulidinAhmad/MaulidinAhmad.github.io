23e6a2c1747d4f913932a3b4279c40b455722ad5
Ahmad Maulidin Al Furqon <maul@Ahmads-MacBook-Pro.local>
2023-06-26 10:47:37 +0700

    change image load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{" "}
             <img
-              src='/vercel.svg'
+              src='vercel.svg'
               alt='Vercel Logo'
               className={styles.vercelLogo}
               width={100}
@@ -31,7 +31,7 @@ export default function Home() {
       <div className={styles.center}>
         <img
           className={styles.logo}
-          src='/next.svg'
+          src='next.svg'
           alt='Next.js Logo'
           width={180}
           height={37}
